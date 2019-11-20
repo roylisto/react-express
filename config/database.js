fe95f6917be9dbfd6df030a1b29ee890f965a17c
Roylisto <roylisto.perdana@finaccel.co>
2019-11-20 13:59:07 +0700

    add port to database setting

--- config/database.js
+++ config/database.js
@@ -5,5 +5,6 @@ module.exports = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
-  dialect: 'mysql',   
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
 }
